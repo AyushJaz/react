677378f77fa0c33fdde08e9819b0a6041ec10f3b
AyushJaz <ajayush2004@gmail.com>
2024-06-25 19:44:51 +0530

    custom react

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,1 +1,29 @@
-const main=document.getElementById('root'); 
+function customRender(reactElement,mainContainer){
+
+  /*------------trial approach---------*/
+//   const newElement = document.createElement(reactElement.type);
+//   newElement.setAttribute('href',reactElement.props.href);
+//   newElement.setAttribute('target',reactElement.props.target);
+//   newElement.innerHTML=reactElement.children;
+//   mainContainer.appendChild(newElement);
+
+const newElement=document.createElement(reactElement.type);
+newElement.innerHTML= 'Click it to visite google';
+for (const key in reactElement.props) {
+  newElement.setAttribute(key,reactElement.props[key]);
+}
+mainContainer.appendChild(newElement);
+}
+
+
+const reactElement={
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank',
+    },
+    children:'Click me to visit google'
+}
+const mainContainer=document.getElementById('root'); 
+ 
+customRender(reactElement,mainContainer);
